daa14d5b1ab353e6c6a14f96df9a42ac8c765e01
49rohan <142834725+49rohan@users.noreply.github.com>
2025-01-14 12:45:06 -0600

    ... - Added IncidentController in Controllers - Added a Incident folder in Views - Added a List page in Incident folder - Added a AddEdit page in Incident folder

---
+++ SportsPro/Controllers/IncidentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsPro.Controllers
+{
+    public class IncidentController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
