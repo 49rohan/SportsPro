70635d088d7e1c0d002e05a98e8dd71873eb7dbb
49rohan <142834725+49rohan@users.noreply.github.com>
2025-01-16 13:00:12 -0600

    ... - Updated IncidentController to list out the incidents - Updated List page to list out the incidents according to the assignment - Updated NuGet packages - Updated connection string

--- SportsPro/Controllers/IncidentController.cs
+++ SportsPro/Controllers/IncidentController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsPro.Models;
+using System.Linq;
 
 namespace SportsPro.Controllers
 {
     public class IncidentController : Controller
     {
-        public IActionResult List()
+        private SportsProContext context;
+        public IncidentController(SportsProContext context)
         {
-            return View();
+            this.context = context;
         }
+
+        public IActionResult List() 
+        {
+            var incidents = context.Incidents.Include(i => i.Customer).Include(i => i.Product).OrderBy(i =>i.Title).ToList();
+            return View(incidents);
+        }
+
     }
 }
