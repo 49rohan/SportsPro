0c13c51acde6050217e57e8fd7146e1ae7452ad3
andyyong11 <yongandy07@gmail.com>
2025-04-06 12:39:40 -0500

    fixed errors when merged

--- SportsPro/Models/Customer.cs
+++ SportsPro/Models/Customer.cs
@@ -1,55 +1,23 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace SportsPro.Models
 {
-
     public class Customer
     {
         public int CustomerID { get; set; }
-        // Add this property to your existing Customer class
-        public ICollection<Registration> Registrations { get; set; } = new List<Registration>();
-
-        [Required(ErrorMessage = "First Name is required")]
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "First Name must be between 1 and 50 characters")]
         public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Last Name is required")]
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "Last Name must be between 1 and 50 characters")]
         public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Address is required")]
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "Address must be between 1 and 50 characters")]
         public string Address { get; set; }
-
-        [Required(ErrorMessage = "City is required")]
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "City must be between 1 and 50 characters")]
         public string City { get; set; }
-
-        [Required(ErrorMessage = "State is required")]
-        [StringLength(50, MinimumLength = 1, ErrorMessage = "State must be between 1 and 50 characters")]
         public string State { get; set; }
-
-        [Required(ErrorMessage = "Postal Code is required")]
-        [StringLength(20, MinimumLength = 1, ErrorMessage = "Postal Code must be between 1 and 20 characters")]
         public string PostalCode { get; set; }
-
-        [Required(ErrorMessage = "Country is required")]
         public string CountryID { get; set; }
-        public Country Country { get; set; }
-
-        [RegularExpression(@"\(\d{3}\)[ ]?\d{3}-\d{4}", ErrorMessage = "Phone must be in format (999) 999-9999")]
         public string Phone { get; set; }
-
-
-        [DataType(DataType.EmailAddress)]
-        [StringLength(50, ErrorMessage = "Email must be less than 51 characters")]
         public string Email { get; set; }
 
-        public string FullName => FirstName + " " + LastName;
+        public List<Registration> Registrations { get; set; }
 
-        // Add this property to your existing Customer class
-        
-
-        }
+       
+        public string FullName => $"{FirstName} {LastName}";
+    }
 }
--- SportsPro/Models/Product.cs
+++ SportsPro/Models/Product.cs
@@ -1,28 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsPro.Models
 {
-	public class Product
-	{
-		public int ProductID { get; set; }
-
-        // Add this property to your existing Customer class
-        public ICollection<Registration> Registrations { get; set; } = new List<Registration>();
-
-        [Required]
-		public string ProductCode { get; set; }
-
-		[Required]
-		public string Name { get; set; }
-
-		[Range(0, 1000000)]
-		[Column(TypeName = "decimal(8,2)")]
-		public decimal YearlyPrice { get; set; }
-
-		public DateTime ReleaseDate { get; set; } = DateTime.Now;
-
-	}
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductCode { get; set; }
+        public string Name { get; set; }
+        public decimal YearlyPrice { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        
+        public List<Registration> Registrations { get; set; }
+    }
 }
