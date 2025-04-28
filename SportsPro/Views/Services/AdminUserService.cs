0a0fe62adec145e6b8bf583f24df77106f5e92c3
samharris0509 <156223013+samharris0509@users.noreply.github.com>
2025-04-27 21:49:06 -0500

    sam

---
+++ SportsPro/Views/Services/AdminUserService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace SportsPro.Services
+{
+    public class AdminUserService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminUserService(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task CreateAdminUserAsync()
+        {
+            // Create Admin role if it doesn't exist
+            if (!await _roleManager.RoleExistsAsync("Admin"))
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            // Create Admin user if it doesn't exist
+            var adminUser = await _userManager.FindByNameAsync("admin");
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser
+                {
+                    UserName = "admin",
+                    Email = "admin@sportspro.com",
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(adminUser, "P@ssw0rd");
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+    }
+}
